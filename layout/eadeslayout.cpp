b48bd9c3e094cbc6cdc068f6724cc08989684a46
Tim Angus <tim@ngus.net>
2013-08-28 13:25:47 +0100

    Add lnTaylorSeries

--- layout/eadeslayout.cpp
+++ layout/eadeslayout.cpp
@@ -3,12 +3,37 @@
 
 #include "../utils.h"
 
+static float lnTaylorSeries( float x, int order )
+{
+  int   i, j, k;
+  float result = 0.0f;
+  float xMinus1 = x - 1.0f;
+  float xPlus1  = x + 1.0f;
+  float term = xMinus1 / xPlus1;
+  float y;
+
+  for( i = 0; i <= order; i++ )
+  {
+    k = i * 2 + 1;
+
+    y = term;
+
+    for( j = 1; j < k; j++ )
+      y *= term;
+
+    result += y * ( 1.0f / (float)k );
+  }
+
+  return 2.0f * result;
+}
+
 #define TAYLOR_LNX(x) (2.0f*(((x)-1.0f)/((x)+1.0f)))
 
 #define FACTOR        0.6f
 
 #define REPULSE(x)    (FACTOR*(1.0f/((x)*(x))))
 #define REPULSE_SQ(x) (FACTOR*(1.0f/(x)))
+//#define ATTRACT(x)    (FACTOR*lnTaylorSeries(x, 4))
 #define ATTRACT(x)    (FACTOR*TAYLOR_LNX(x))
 
 void EadesLayout::executeReal()
