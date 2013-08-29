460bc514753811f22e3d945412ad257f92439bb3
Tim Angus <tim@ngus.net>
2013-08-29 16:51:21 +0100

    Add some text about HLT

--- graph/simplecomponentmanager.h
+++ graph/simplecomponentmanager.h
@@ -9,6 +9,25 @@
 #include <QList>
 #include <QSet>
 
+/*
+This class is somewhat sub-optimal in that it simply does a depth first
+search on the graph resulting in O(n) performance. In the static case this
+is perfectly acceptable as it only needs to be performed once, but in the
+dynamic case we potentially have many graph changes in a short period of
+time meaning that a graph's component configuration may change frequently.
+
+O(log^2 n) performance is achievable using the Holm, de Lichtenberg and
+Thorup algorithm explained in the paper "Poly-Logarithmic Deterministic
+Fully-Dynamic Algorithms for Connectivity, Minimum Spanning Tree, 2-Edge,
+and Biconnectivity". So the plan is to implement this as a replacement
+for SimpleComponentManager in future.
+
+Some links:
+http://www.mpi-inf.mpg.de/departments/d1/teaching/ss12/AdvancedGraphAlgorithms/Slides08.pdf
+http://courses.csail.mit.edu/6.851/spring07/scribe/lec05.pdf
+http://www.youtube.com/watch?v=5NEzZPYs04c#t=3076
+*/
+
 class SimpleComponentManager : public ComponentManager
 {
     Q_OBJECT
