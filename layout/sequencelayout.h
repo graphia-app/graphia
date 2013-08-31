83fdc889ee81ba092b673d4733315d7718fe80e4
Tim Angus <tim@ngus.net>
2013-08-31 23:13:06 +0100

    Add SequenceLayout

---
+++ layout/sequencelayout.h
@@ -0,0 +1,43 @@
+#ifndef SEQUENCELAYOUT_H
+#define SEQUENCELAYOUT_H
+
+#include "layout.h"
+
+#include <QList>
+
+class SequenceLayout : public Layout
+{
+    Q_OBJECT
+private:
+    QList<Layout*> subLayouts;
+
+public:
+    SequenceLayout(const ReadOnlyGraph& graph, NodeArray<QVector3D>& positions) :
+        Layout(graph, positions)
+    {}
+
+    SequenceLayout(const ReadOnlyGraph& graph, NodeArray<QVector3D>& positions, QList<Layout*> subLayouts) :
+        Layout(graph, positions), subLayouts(subLayouts)
+    {}
+
+    void addSubLayout(Layout* layout) { subLayouts.append(layout); }
+
+    bool iterative()
+    {
+        for(Layout* subLayout : subLayouts)
+        {
+            if(subLayout->iterative())
+                return true;
+        }
+
+        return false;
+    }
+
+    void executeReal()
+    {
+        for(Layout* subLayout : subLayouts)
+            subLayout->execute();
+    }
+};
+
+#endif // SEQUENCELAYOUT_H
