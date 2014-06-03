32a48fae5ecb600e5ab0aea3a873a10a9dcecb69
Tim Angus <tim@ngus.net>
2014-06-03 17:25:42 +0100

    Don't use CentreLayout in combination with EadesLayout anymore; the renderer does it anyway and it interferes with node positions

--- source/layout/eadeslayout.h
+++ source/layout/eadeslayout.h
@@ -33,14 +33,7 @@ public:
     NodeLayout* create(ComponentId componentId) const
     {
         const ReadOnlyGraph* graph = this->_graphModel->graph().componentById(componentId);
-        EadesLayout* eadesLayout = new EadesLayout(*graph, this->_graphModel->nodePositions());
-        CentreingLayout* centreingLayout = new CentreingLayout(*graph, this->_graphModel->nodePositions());
-
-        QList<NodeLayout*> subLayouts;
-        subLayouts.append(eadesLayout);
-        subLayouts.append(centreingLayout);
-        SequenceLayout* sequenceLayout = new SequenceLayout(*graph, this->_graphModel->nodePositions(), subLayouts);
-        return sequenceLayout;
+        return new EadesLayout(*graph, this->_graphModel->nodePositions());
     }
 };
 
