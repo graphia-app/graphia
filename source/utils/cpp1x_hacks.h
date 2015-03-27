578344da79a9d2b0895a39d13a20f9a0033d6d5d
Tim Angus <tim@ngus.net>
2015-03-27 12:17:15 +0000

    Turns out unique_ptr requires <memory>

--- source/utils/cpp1x_hacks.h
+++ source/utils/cpp1x_hacks.h
@@ -3,12 +3,15 @@
 
 #ifndef _MSC_VER
 #if __cplusplus <= 201103L
-namespace std {
-template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args&& ...args )
+#include <memory>
+
+namespace std
 {
-    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
-}
+    template<typename T, typename ...Args>
+    std::unique_ptr<T> make_unique( Args&& ...args )
+    {
+        return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+    }
 }
 #else
 #error std::make_unique should now be available
