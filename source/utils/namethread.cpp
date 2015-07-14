da7bcc813c5a535320f626fc544f9ea55a141b31
Tim Angus <tim@ngus.net>
2015-07-14 10:32:11 +0100

    Implement nameCurrentThread on Windows

--- source/utils/namethread.cpp
+++ source/utils/namethread.cpp
@@ -20,6 +20,47 @@ const QString currentThreadName()
     return threadName;
 }
 
+#elif _WIN32
+#include <windows.h>
+const DWORD MS_VC_EXCEPTION  =0x406D1388;
+
+#pragma pack(push,8)
+typedef struct tagTHREADNAME_INFO
+{
+    DWORD dwType; // Must be 0x1000.
+    LPCSTR szName; // Pointer to name (in user addr space).
+    DWORD dwThreadID; // Thread ID (-1=caller thread).
+    DWORD dwFlags; // Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+
+void SetThreadName(char* threadName)
+{
+    THREADNAME_INFO info;
+    info.dwType = 0x1000;
+    info.szName = threadName;
+    info.dwThreadID = -1;
+    info.dwFlags = 0;
+
+    __try
+    {
+        RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+    }
+    __except(EXCEPTION_EXECUTE_HANDLER)
+    {
+    }
+}
+
+void nameCurrentThread(const QString& name)
+{
+    SetThreadName(name.toLatin1().data());
+}
+
+const QString currentThreadName()
+{
+    // Windows doesn't really have a concept of named threads (see above), so use the ID instead
+    return QString::number(Utils::currentThreadId());
+}
 #else
 void nameCurrentThread(const QString&) {}
 const QString currentThreadName()
