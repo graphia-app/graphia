d6c4c0ff78dc5dddfcd8ed5d9f856e44992a586d
Tim Angus <tim@ngus.net>
2015-07-18 23:28:55 +0100

    ...debug output not available in release builds

--- source/main.cpp
+++ source/main.cpp
@@ -16,36 +16,36 @@
 
 int main(int argc, char *argv[])
 {
-    qDebug() << "1";
+    std::cout << "1";
     ThreadPool threadPool;
-    qDebug() << "2";
+    std::cout << "2";
     QApplication app(argc, argv);
-    qDebug() << "3";
+    std::cout << "3";
 
     // Since Qt is responsible for managing OpenGL, we need
     // to give it a hint that we want debug context
     if(qgetenv("OPENGL_DEBUG").toInt() > 0)
         qputenv("QSG_OPENGL_DEBUG", "1");
 
-    qDebug() << "4";
+    std::cout << "4";
 #ifndef Q_OS_LINUX
-    qDebug() << "1";
+    std::cout << "1";
     QIcon::setThemeName("Tango");
 #endif
 
-    qDebug() << "5";
+    std::cout << "5";
     bool hasOpenGLSupport = OpenGLFunctions::checkOpenGLSupport();
     Q_UNUSED(hasOpenGLSupport); //FIXME do something with this
 
-    qDebug() << "6";
+    std::cout << "6";
     qmlRegisterType<Application>("com.kajeka", 1, 0, "Application");
     qmlRegisterType<Document>("com.kajeka", 1, 0, "Document");
     qmlRegisterType<GraphQuickItem>("com.kajeka", 1, 0, "Graph");
 
-    qDebug() << "7";
+    std::cout << "7";
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:///qml/main.qml")));
 
-    qDebug() << "8";
+    std::cout << "8";
     return app.exec();
 }
