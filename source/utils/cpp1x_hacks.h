7f9bc0a4b419dddbc6147775952fe3cbdb3914d3
Tim Angus <tim@ngus.net>
2015-08-31 10:24:34 +0100

    Formatting

--- source/utils/cpp1x_hacks.h
+++ source/utils/cpp1x_hacks.h
@@ -8,9 +8,9 @@
 namespace std
 {
     template<typename T, typename ...Args>
-    std::unique_ptr<T> make_unique( Args&& ...args )
+    std::unique_ptr<T> make_unique(Args&& ...args)
     {
-        return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
     }
 }
 #else
