4c6188fdb4e12e072adce48bc7c876d002bdefa9
Tim Angus <tim@ngus.net>
2015-10-14 14:40:30 +0100

    update() the graph between transform applications

--- source/transform/compoundtransform.cpp
+++ source/transform/compoundtransform.cpp
@@ -1,5 +1,7 @@
 #include "compoundtransform.h"
 
+#include "transformedgraph.h"
+
 void CompoundTransform::apply(const Graph& source, TransformedGraph& target) const
 {
     if(_transforms.empty())
@@ -7,10 +9,15 @@ void CompoundTransform::apply(const Graph& source, TransformedGraph& target) con
 
     // We can only use the apply overload with a source for the first transformation...
     _transforms.front()->apply(source, target);
+    target.update();
 
     // ...thereafter we use the inplace one
     for(auto i = _transforms.begin() + 1; i != _transforms.end(); i++)
-        (*i)->apply(target);
+    {
+        auto& transform = *i;
+        transform->apply(target);
+        target.update();
+    }
 }
 
 void CompoundTransform::apply(TransformedGraph& target) const
@@ -19,5 +26,8 @@ void CompoundTransform::apply(TransformedGraph& target) const
         return;
 
     for(auto& transform : _transforms)
+    {
         transform->apply(target);
+        target.update();
+    }
 }
