e51966d183e72656513de82bb0397c8c6567f642
Tim Angus <tim@ngus.net>
2016-03-29 13:15:38 +0100

    Move .app creation out of Jenkins

---
+++ installers/osx/build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+security unlock-keychain -p ${SIGN_BUILD_USER_PASSWORD}
+macdeployqt ${PRODUCT_NAME}.app -qmldir=source/ui/qml \
+  -executable=${PRODUCT_NAME}.app/Contents/MacOS/${PRODUCT_NAME} \
+  -codesign="${SIGN_APPLE_KEYCHAIN_ID}"
+
+rm -rf build
+mkdir build
+cp -a ${PRODUCT_NAME}.app build
