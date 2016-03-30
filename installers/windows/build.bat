11825cbca77e0dccc2e198bc71f39f7ea6ee202e
Tim Angus <tim@ngus.net>
2016-03-30 13:31:23 +0100

    Add EstimatedSize uninstaller key

--- installers/windows/build.bat
+++ installers/windows/build.bat
@@ -11,8 +11,18 @@ xcopy "%UniversalCRTSdkDir%redist\ucrt\DLLs\x64\*.*" build || EXIT /B 1
 signtool sign /f %SIGN_KEYSTORE_WINDOWS% /p %SIGN_PASSWORD% ^
 	/tr %SIGN_TSA% /td SHA256 build\%PRODUCT_NAME%.exe || EXIT /B 1
 
+@echo off
+setLocal EnableDelayedExpansion
+set /a value=0
+set /a BUILD_SIZE=0
+FOR /R %1 %%I IN (build\*) DO (
+set /a value=%%~zI/1024
+set /a BUILD_SIZE=!BUILD_SIZE!+!value!
+)
+@echo on
+
 makensis /NOCD /DPRODUCT_NAME=%PRODUCT_NAME% /DVERSION=%VERSION% ^
-	/DPUBLISHER="%PUBLISHER%" /DCOPYRIGHT="%COPYRIGHT%" ^
+	/DPUBLISHER="%PUBLISHER%" /DCOPYRIGHT="%COPYRIGHT%" /DBUILD_SIZE=%BUILD_SIZE% ^
 	"/XOutFile build\%PRODUCT_NAME%-%VERSION%-installer.exe" ^
 	installers\windows\installer.nsi
 
