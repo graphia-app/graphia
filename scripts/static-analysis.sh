5e39216a819b415119a2f922e5fed8b9d7c13df0
Tim Angus <tim@ngus.net>
2017-03-27 16:00:06 +0100

    static-analysis.sh tweaks

--- scripts/static-analysis.sh
+++ scripts/static-analysis.sh
@@ -8,12 +8,16 @@ find source/app \
   source/plugins \
   source/crashreporter \
   -iname "*.cpp" -not -iname "moc_*" -not -iname "qrc_*" | \
-  xargs cppcheck --enable=all --inconclusive \
+  xargs cppcheck --enable=all \
   --xml --xml-version=2 \
   --library=scripts/cppcheck.cfg 2> cppcheck.xml
 
-# clang-tidy (this assumes a compile_command.json has been created by bear)
-CHECKS="-checks=*,-*readability*,-llvm-*,-google-*"
+# clang-tidy (this works better when a compile_command.json has been created by bear)
+CHECKS="-checks=*, \
+  -*readability*, \
+  -llvm-*, \
+  -google-*, \
+  -clang-analyzer-alpha.deadcode.UnreachableCode"
 
 clang-tidy -list-checks ${CHECKS}
 find source/app \
