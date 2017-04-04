ad91e97f9a4bff7ac0d0de820c41c1c4e8b9557c
Tim Angus <tim@ngus.net>
2017-04-04 11:34:37 +0100

    Fix Windows build

--- scripts/windows-build.bat
+++ scripts/windows-build.bat
@@ -4,20 +4,19 @@ set BUILD_DIR=build
 rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
 
-jom distclean || echo "distclean failed"
-
 cd %BUILD_DIR%
 qmake -version || EXIT /B 1
 qmake ..\GraphTool.pro || EXIT /B 1
 jom || EXIT /B 1
 jom clean || EXIT /B 1
+cd ..
 
 setlocal EnableDelayedExpansion
 
-..\source\thirdparty\breakpad\src\tools\windows\binaries\dump_syms.exe ^
-	%PRODUCT_NAME%.exe > %PRODUCT_NAME%.sym
+source\thirdparty\breakpad\src\tools\windows\binaries\dump_syms.exe ^
+	%BUILD_DIR%\%PRODUCT_NAME%.exe > %BUILD_DIR%\%PRODUCT_NAME%.sym
 
-FOR %%f IN (plugins\*.dll) DO (
-	..\source\thirdparty\breakpad\src\tools\windows\binaries\dump_syms.exe ^
+FOR %%f IN (%BUILD_DIR%\plugins\*.dll) DO (
+	source\thirdparty\breakpad\src\tools\windows\binaries\dump_syms.exe ^
 		%%f > %%~df%%~pf%%~nf.sym
 )
