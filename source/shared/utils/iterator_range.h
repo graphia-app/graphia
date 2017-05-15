6f1c6c371945e0be47fcd07f52e055dc45ff9c01
Tim Angus <tim@ngus.net>
2017-05-15 16:59:58 +0100

    Revert "Improve make_iterator_range"
    This reverts commit df2feafbb48511881c3c1fd02ac1f67805d3139f.

--- source/shared/utils/iterator_range.h
+++ source/shared/utils/iterator_range.h
@@ -1,37 +1,24 @@
 #ifndef ITERATOR_RANGE_H
 #define ITERATOR_RANGE_H
 
-#include <type_traits>
-
-template<typename BeginIt, typename EndIt>
-class iterator_range
+template<typename It> class iterator_range
 {
 public:
-    iterator_range(BeginIt begin, EndIt end) :
+    iterator_range(It begin, It end) :
         _begin(begin), _end(end) {}
 
-    BeginIt& begin() const { return _begin; }
-    EndIt& end() const { return _end; }
+    It begin() const { return _begin; }
+    It end() const { return _end; }
 
 private:
-    BeginIt _begin;
-    EndIt _end;
+    It _begin;
+    It _end;
 };
 
-// This converts an rvalue reference to a value,
-// and everything else to an lvalue reference
-template<typename T>
-using rv2v = std::conditional_t
-<
-    std::is_rvalue_reference<T>::value,
-    std::remove_reference_t<T>,
-    std::add_lvalue_reference_t<std::decay_t<T>>
->;
-
-template<typename BeginIt, typename EndIt>
-auto make_iterator_range(BeginIt&& begin, EndIt&& end)
+template<typename It>
+iterator_range<It> make_iterator_range(It begin, It end)
 {
-    return iterator_range<rv2v<BeginIt>, rv2v<EndIt>>(begin, end);
+    return iterator_range<It>(begin, end);
 }
 
 #endif // ITERATOR_RANGE_H
