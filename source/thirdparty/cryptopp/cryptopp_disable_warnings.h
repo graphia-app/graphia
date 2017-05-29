c68c044fa9c13116f2a02392160fca54c2decc36
Tim Angus <tim@ngus.net>
2017-05-29 18:10:55 +0100

    Add cryptopp

---
+++ source/thirdparty/cryptopp/cryptopp_disable_warnings.h
@@ -0,0 +1,21 @@
+// cryptopp is a very warning noisy library, so we allow some warnings to be disabled
+// by including this header before including any crypto headers. The companion to
+// this is crypto_enable_warnings.h which should be included as soon as possible
+// after the affected code; especially if we're including from inside another header.
+
+#define GCC_DIAGNOSTIC_AWARE \
+    ((__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2)) || \
+    defined(__clang__))
+
+#if GCC_DIAGNOSTIC_AWARE
+// GCC/clang warnings
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
+#ifdef _MSC_VER
+// MSVC warnings
+#pragma warning( push )
+#endif
