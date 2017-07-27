29d9c16c4dda95748208869158b506a68b69a226
Tim Angus <tim@ngus.net>
2017-07-27 16:56:51 +0100

    Add QmlUtils::replaceExtension

--- source/shared/utils/qmlutils.h
+++ source/shared/utils/qmlutils.h
@@ -14,6 +14,17 @@ public:
     Q_INVOKABLE QString fileNameForUrl(const QUrl& url) const { return url.toLocalFile(); }
     Q_INVOKABLE QUrl urlForFileName(const QString& fileName) const { return QUrl::fromLocalFile(fileName); }
     Q_INVOKABLE bool fileUrlExists(const QUrl& url) const { return QFileInfo(url.toLocalFile()).exists(); }
+
+    Q_INVOKABLE QUrl replaceExtension(const QUrl& url, const QString& extension) const
+    {
+        auto fi = QFileInfo(url.toLocalFile());
+        auto replaced = QFileInfo(QString("%1/%2.%3")
+                                  .arg(fi.path())
+                                  .arg(fi.baseName())
+                                  .arg(extension));
+
+        return QUrl::fromLocalFile(replaced.filePath());
+    }
 };
 
 #endif // QMLUTILS_H
