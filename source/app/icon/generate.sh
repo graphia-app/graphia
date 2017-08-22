5b822a6536280f54c35247a9e5edafb85cbb6570
Tim Angus <tim@ngus.net>
2017-08-22 14:38:27 +0100

    Make icon generation script more generic

--- source/app/icon/generate.sh
+++ source/app/icon/generate.sh
@@ -2,16 +2,25 @@
 
 SCRIPT=$(readlink -f $0)
 SCRIPT_DIR=$(dirname ${SCRIPT})
+ICON_FILE=$1
+
+if [ ! -e ${ICON_FILE} ]
+then
+  echo ${ICON_FILE} does not exist!
+  exit 1
+fi
+
+BASE_NAME="${ICON_FILE%.*}"
 
 for SIZE in 1024 512 256 128 64 48 32 16
 do
-  rsvg -w ${SIZE} -h ${SIZE} Icon.svg Icon${SIZE}x${SIZE}.png
+  rsvg -w ${SIZE} -h ${SIZE} ${BASE_NAME}.svg ${BASE_NAME}${SIZE}x${SIZE}.png
 done
 
 function iconFilesOfSize()
 {
   PNGS=
-  for PNG in $(ls ${SCRIPT_DIR}/*.png);
+  for PNG in $(ls ${SCRIPT_DIR}/${BASE_NAME}*.png);
   do
     VALID_SIZES="$1"
     SIZE=$(convert ${PNG} -print "%w\n" /dev/null)
@@ -25,5 +34,5 @@ function iconFilesOfSize()
   echo ${PNGS}
 }
 
-icotool -c $(iconFilesOfSize "256 128 64 48 32 16") -o ${SCRIPT_DIR}/Icon.ico
-png2icns ${SCRIPT_DIR}/Icon.icns $(iconFilesOfSize "1024 512 256 128 32 16")
+icotool -c $(iconFilesOfSize "256 128 64 48 32 16") -o ${SCRIPT_DIR}/${BASE_NAME}.ico
+png2icns ${SCRIPT_DIR}/${BASE_NAME}.icns $(iconFilesOfSize "1024 512 256 128 32 16")
