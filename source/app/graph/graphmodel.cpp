9e9807f914138efd7002863d82c27d4fd35e6e97
Tim Angus <tim@ngus.net>
2017-10-27 13:08:11 +0100

    Add Node In/Out Degree attributes

--- source/app/graph/graphmodel.cpp
+++ source/app/graph/graphmodel.cpp
@@ -66,6 +66,16 @@ GraphModel::GraphModel(QString name, IPlugin* plugin) :
         .intRange().setMin(0)
         .setDescription(tr("A node's degree is its number of incident edges."));
 
+    GraphModel::createAttribute(tr("Node In Degree"))
+        .setIntValueFn([this](NodeId nodeId) { return _transformedGraph.nodeById(nodeId).inEdgeIds().size(); })
+        .intRange().setMin(0)
+        .setDescription(tr("A node's in degree is its number of inbound edges."));
+
+    GraphModel::createAttribute(tr("Node Out Degree"))
+        .setIntValueFn([this](NodeId nodeId) { return _transformedGraph.nodeById(nodeId).outEdgeIds().size(); })
+        .intRange().setMin(0)
+        .setDescription(tr("A node's out degree is its number of outbound edges."));
+
     GraphModel::createAttribute(tr("Node Multiplicity"))
         .setIntValueFn([this](NodeId nodeId)
         {
