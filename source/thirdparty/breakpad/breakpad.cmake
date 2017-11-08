a7cab3c5ef5d07781745862d86e98488cd0fef55
Tim Angus <tim@ngus.net>
2017-11-08 15:09:37 +0000

    Fix Linux breakpad build

--- source/thirdparty/breakpad/breakpad.cmake
+++ source/thirdparty/breakpad/breakpad.cmake
@@ -7,29 +7,29 @@ if(UNIX)
         ${CMAKE_CURRENT_LIST_DIR}/src/common/md5.cc
         ${CMAKE_CURRENT_LIST_DIR}/src/common/string_conversion.cc
     )
-endif()
 
-if(LINUX)
-    list(APPEND SOURCES
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/crash_generation/crash_generation_client.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/dump_writer_common/thread_info.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/dump_writer_common/ucontext_reader.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/handler/exception_handler.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/handler/minidump_descriptor.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/log/log.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/microdump_writer/microdump_writer.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_core_dumper.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_dumper.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_ptrace_dumper.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/minidump_writer.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/elf_core_dump.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/elfutils.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/file_id.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/guid_creator.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/linux_libc_support.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/memory_mapped_file.cc
-        ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/safe_readlink.cc
-    )
+    if(NOT APPLE)
+        list(APPEND SOURCES
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/crash_generation/crash_generation_client.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/dump_writer_common/thread_info.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/dump_writer_common/ucontext_reader.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/handler/exception_handler.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/handler/minidump_descriptor.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/log/log.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/microdump_writer/microdump_writer.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_core_dumper.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_dumper.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/linux_ptrace_dumper.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/client/linux/minidump_writer/minidump_writer.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/elf_core_dump.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/elfutils.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/file_id.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/guid_creator.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/linux_libc_support.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/memory_mapped_file.cc
+            ${CMAKE_CURRENT_LIST_DIR}/src/common/linux/safe_readlink.cc
+        )
+    endif()
 endif()
 
 if(APPLE)
