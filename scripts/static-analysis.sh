d1f66115aeb33db3a7e5c4bcb77aa5f928b228c6
Tim Angus <tim@ngus.net>
2017-11-29 13:59:08 +0000

    No

--- scripts/static-analysis.sh
+++ scripts/static-analysis.sh
@@ -16,63 +16,59 @@ NUM_CORES=$(nproc --all)
 COMPILER=$(basename ${CC} | sed -e 's/-.*//g')
 BUILD_DIR="build/${COMPILER}"
 
-(
-  cd ${BUILD_DIR}
+CPP_FILES=$(cat ${BUILD_DIR}/compile_commands.json | \
+  jq '.[].file' | grep -vE "qrc_|mocs_compilation|thirdparty" | \
+  sed -e 's/"//g')
 
-  CPP_FILES=$(cat compile_commands.json | \
-    jq '.[].file' | grep -vE "qrc_|mocs_compilation|thirdparty" | \
-    sed -e 's/"//g')
+echo "Files to be analysed:"
+echo ${CPP_FILES}
 
-  echo "Files to be analysed:"
-  echo ${CPP_FILES}
+# cppcheck
+cppcheck --version
+cppcheck --enable=all --xml --xml-version=2 \
+  --library=scripts/cppcheck.cfg ${CPP_FILES} 2> cppcheck.xml
 
-  # cppcheck
-  cppcheck --version
-  cppcheck --enable=all --xml --xml-version=2 \
-    --library=scripts/cppcheck.cfg ${CPP_FILES} 2> cppcheck.xml
+# clang-tidy
+CHECKS="-checks=*,\
+-readability-braces-around-statements,\
+-readability-identifier-naming,\
+-readability-named-parameter,\
+-llvm-*,\
+llvm-namespace-comment,\
+-google-*,\
+google-explicit-constructor,\
+google-runtime-int,\
+google-runtime-member-string-references,\
+-cppcoreguidelines-pro-type-vararg,\
+-clang-diagnostic-unknown-warning-option,\
+-clang-analyzer-alpha.deadcode.UnreachableCode"
 
-  # clang-tidy
-  CHECKS="-checks=*,\
-  -readability-braces-around-statements,\
-  -readability-identifier-naming,\
-  -readability-named-parameter,\
-  -llvm-*,\
-  llvm-namespace-comment,\
-  -google-*,\
-  google-explicit-constructor,\
-  google-runtime-int,\
-  google-runtime-member-string-references,\
-  -cppcoreguidelines-pro-type-vararg,\
-  -clang-diagnostic-unknown-warning-option,\
-  -clang-analyzer-alpha.deadcode.UnreachableCode"
+echo "clang-tidy"
+clang-tidy --version
+clang-tidy -list-checks ${CHECKS}
+parallel -n1 -P${NUM_CORES} -q \
+  clang-tidy -p ${BUILD_DIR} \
+  -header-filter="^.*source\/(app|shared|plugins).*$" ${CHECKS} {} \
+  ::: ${CPP_FILES}
 
-  echo "clang-tidy"
-  clang-tidy --version
-  clang-tidy -list-checks ${CHECKS}
-  parallel -n1 -P${NUM_CORES} -q \
-    clang-tidy \
-    -header-filter="^.*source\/(app|shared|plugins).*$" ${CHECKS} {} \
-    ::: ${CPP_FILES}
+# clazy
+CHECKS="-checks=level1,\
+base-class-event,\
+container-inside-loop,\
+global-const-char-pointer,\
+implicit-casts,\
+missing-typeinfo,\
+qstring-allocations,\
+reserve-candidates,\
+no-rule-of-two-soft,\
+no-qenums,\
+no-non-pod-global-static"
 
-  # clazy
-  CHECKS="-checks=level1,\
-  base-class-event,\
-  container-inside-loop,\
-  global-const-char-pointer,\
-  implicit-casts,\
-  missing-typeinfo,\
-  qstring-allocations,\
-  reserve-candidates,\
-  no-rule-of-two-soft,\
-  no-qenums,\
-  no-non-pod-global-static"
-
-  echo "clazy"
-  clazy-standalone --version
-  parallel -n1 -P${NUM_CORES} \
-    clazy-standalone -p compile_commands.json ${CHECKS} {} \
-    ::: ${CPP_FILES}
-)
+echo "clazy"
+clazy-standalone --version
+parallel -n1 -P${NUM_CORES} \
+  clazy-standalone -p ${BUILD_DIR}/compile_commands.json ${CHECKS} {} \
+  ::: ${CPP_FILES}
 
 # qmllint
 qmllint --version
