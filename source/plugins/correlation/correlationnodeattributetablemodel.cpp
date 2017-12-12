bdb2d52f85c56a86a5cfa6e5c09e498e2e819a16
Tim Angus <tim@ngus.net>
2017-12-12 17:21:03 +0000

    Fix clazy warning

--- source/plugins/correlation/correlationnodeattributetablemodel.cpp
+++ source/plugins/correlation/correlationnodeattributetablemodel.cpp
@@ -8,6 +8,7 @@ QStringList CorrelationNodeAttributeTableModel::columnNames() const
 
     if(_dataColumnNames != nullptr)
     {
+        list.reserve(list.size() + _dataColumnNames->size());
         for(const auto& dataColumnName : *_dataColumnNames)
             list.append(dataColumnName);
     }
