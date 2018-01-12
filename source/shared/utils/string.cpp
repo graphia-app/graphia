f2495a9e659282b638ab79148ba83804798ea7e4
Tim Angus <tim@ngus.net>
2018-01-12 16:33:23 +0000

    Fix u::formatUsingSIPostfix

--- source/shared/utils/string.cpp
+++ source/shared/utils/string.cpp
@@ -1,6 +1,7 @@
 #include "string.h"
 
 #include <QStringList>
+#include <QRegularExpression>
 
 #include <vector>
 #include <cmath>
@@ -97,7 +98,7 @@ QString u::formatUsingSIPostfix(double number)
         if(number >= v.threshold)
         {
             auto digits = QString::number(number / v.divider, 'f', 1);
-            digits.replace("\\.?0+$", "");
+            digits.replace(QRegularExpression(R"(\.0+$)"), QStringLiteral(""));
 
             return digits + v.symbol;
         }
