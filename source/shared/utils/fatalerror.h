e0ac70d3120dc409c52198eee890e8625352c54d
Tim Angus <tim@ngus.net>
2018-01-15 18:43:25 +0000

    (Try to) Force FATAL_ERROR to not inline

--- source/shared/utils/fatalerror.h
+++ source/shared/utils/fatalerror.h
@@ -1,12 +1,18 @@
 #ifndef FATALERROR_H
 #define FATALERROR_H
 
+#if defined(_MSC_VER)
+#define NOINLINE __declspec(noinline)
+#elif defined(__GNUC__)
+#define NOINLINE __attribute__ ((noinline))
+#endif
+
 // Deliberately crash in order to help track down bugs
 // MESSAGE becomes a struct name, so quotes and spaces are not allowed
 #define FATAL_ERROR(MESSAGE) \
     do { struct MESSAGE \
         { \
-            void operator()() \
+            NOINLINE void operator()() \
             { \
                 int* p = nullptr; \
                 *p = 0; \
