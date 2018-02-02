65b838a053b28d0c0a7dde7e7329297fe59cb133
Tim Angus <tim@ngus.net>
2018-02-02 16:59:15 +0000

    :facepalm:

--- source/shared/plugins/basegenericplugin.cpp
+++ source/shared/plugins/basegenericplugin.cpp
@@ -77,11 +77,11 @@ bool BaseGenericPluginInstance::load(const QByteArray& data, int dataVersion,
     if(!_userNodeData.load(jsonObject["userNodeData"], progressFn))
         return false;
 
-    if(!u::contains(jsonObject, "edgeNodeData") || !jsonObject["edgeNodeData"].is_object())
+    if(!u::contains(jsonObject, "userEdgeData") || !jsonObject["userEdgeData"].is_object())
         return false;
 
     graph.setPhase(QObject::tr("User Edge Data"));
-    if(!_userEdgeData.load(jsonObject["edgeNodeData"], progressFn))
+    if(!_userEdgeData.load(jsonObject["userEdgeData"], progressFn))
         return false;
 
     return true;
