874572d48552ad9d26da4ffae3287909d255b086
Tim Angus <tim@ngus.net>
2018-03-30 17:45:21 +0100

    Remove obsolete json include directory

--- source/thirdparty/thirdparty.cmake
+++ source/thirdparty/thirdparty.cmake
@@ -2,6 +2,5 @@ include_directories(${CMAKE_CURRENT_LIST_DIR})
 include_directories(${CMAKE_CURRENT_LIST_DIR}/blaze)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/boost/spirit)
 include_directories(${CMAKE_CURRENT_LIST_DIR}/json)
-include_directories(${CMAKE_CURRENT_LIST_DIR}/json/src)
 
 add_definitions(-DQCUSTOMPLOT_USE_OPENGL)
