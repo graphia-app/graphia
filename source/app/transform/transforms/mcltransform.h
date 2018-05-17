911d0343a63759378b78cd96a3aef8f45a924c88
Tim Angus <tim@ngus.net>
2018-05-17 10:56:41 +0100

    Fix accidental push

--- source/app/transform/transforms/mcltransform.h
+++ source/app/transform/transforms/mcltransform.h
@@ -44,11 +44,8 @@ public:
 
     GraphTransformParameters parameters() const override
     {
-        return {
-            {"Granularity", {ValueType::Float, QObject::tr("Controls the size of the resultant clusters. "
-            "A larger granularity value results in smaller clusters."), 2.2, 1.1, 3.5}},
-            {"Test", {ValueType::StringList, QObject::tr("sdfsdfs"), QStringList{"A", "B"} }}
-        };
+        return {{"Granularity", {ValueType::Float, QObject::tr("Controls the size of the resultant clusters. "
+            "A larger granularity value results in smaller clusters."), 2.2, 1.1, 3.5}}};
     }
 
     DeclaredAttributes declaredAttributes() const override
