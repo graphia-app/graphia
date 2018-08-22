f7604e80f246b34236e92e01c5d975682e539bdf
Tim Angus <tim@ngus.net>
2018-08-22 11:58:00 +0100

    Make generate-icons-qrc.sh cope with iconSource:

--- source/app/icon-themes/generate-icons-qrc.sh
+++ source/app/icon-themes/generate-icons-qrc.sh
@@ -12,20 +12,28 @@ THEME_FILES=$(find * -name "*.theme")
 ICON_NAMES=$(find $(find ../../ -type d -name "qml") -name "*.qml" | \
   xargs perl -pe 's/\n/\$/g' | \
   perl -ne 'print "$1\n" while /iconName:[\s\$]*(({([^{}]|(?2))*})|([^\$]*))\$/gm' | \
-  perl -pe 's/[^\"]*\"([^\"]*)\"[^\"]*/$1\n/g' | \
+  perl -pe 's/[^\"]*\"([^\"]*)\"[^\"]*/$1\.\*\n/g' | \
   sort | uniq)
+
+ICON_SOURCES=$(find $(find ../../ -type d -name "qml") -name "*.qml" | \
+  xargs perl -pe 's/\n/\$/g' | \
+  perl -ne 'print "$1\n" while /iconSource:[\s\$]*(({([^{}]|(?2))*})|([^\$]*))\$/gm' | \
+  perl -pe 's/[^\"]*\".*\/([^\"]*)\"[^\"]*/$1\n/g' | \
+  sort | uniq)
+
+ICON_SOURCES="${ICON_NAMES} ${ICON_SOURCES}"
 
 echo -e ${PREAMBLE} > icons.qrc
 
-for THEME_FILE in ${THEME_FILES};
+for THEME_FILE in ${THEME_FILES}
 do
   echo -e "\t\t<file>${THEME_FILE}</file>" >> icons.qrc
 done
 
-for ICON_NAME in ${ICON_NAMES};
+for ICON_SOURCE in ${ICON_SOURCES}
 do
-  ICON_FILES=$(find * -iname "${ICON_NAME}.*" | sort)
-  for ICON_FILE in ${ICON_FILES};
+  ICON_FILES=$(find * -iname "${ICON_SOURCE}" | sort)
+  for ICON_FILE in ${ICON_FILES}
   do
     if [ -L ${ICON_FILE} ]
     then
