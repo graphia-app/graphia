f1a3a7a7707285669c973a2e2c8bac1b2f60f45b
Tim Angus <tim@ngus.net>
2018-11-26 15:06:44 +0000

    Add objectsEquivalent to Utils.js

--- source/shared/ui/qml/Utils.js
+++ source/shared/ui/qml/Utils.js
@@ -7,6 +7,25 @@ function watchPropertyChanges(object, handler)
     }
 }
 
+function objectsEquivalent(a, b)
+{
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    if(aProps.length !== bProps.length)
+        return false;
+
+    for(var i = 0; i < aProps.length; i++)
+    {
+        var propName = aProps[i];
+
+        if(a[propName] !== b[propName])
+            return false;
+    }
+
+    return true;
+}
+
 function isNumeric(value)
 {
     return !isNaN(parseFloat(value)) && isFinite(value);
