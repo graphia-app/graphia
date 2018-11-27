6f3376acdb04308276749bcb25c9ed1d76e40209
Tim Angus <tim@ngus.net>
2018-11-27 16:46:38 +0000

    Populate default palette presets

--- source/app/ui/visualisations/defaultpalettes.h
+++ source/app/ui/visualisations/defaultpalettes.h
@@ -4,8 +4,9 @@
 namespace Defaults
 {
 const char* const PALETTE_PRESETS =
-R"([{
-    "autoColors": [
+R"([
+  {
+    "autoColors":[
       "#199311",
       "#FF7700",
       "#8126C0",
@@ -19,12 +20,65 @@ R"([{
       "#798FF0",
       "#111111"
     ],
-    "defaultColor": "#DDDDDD"
-}])";
+    "defaultColor":"#DDDDDD"
+  },
+  {
+    "autoColors":[
+      "#199311",
+      "#FF7700",
+      "#8126C0",
+      "#1DF3F3",
+      "#FFEE33",
+      "#FD1111",
+      "#FF69B4",
+      "#003BFF",
+      "#8AFF1E",
+      "#9D2323",
+      "#798FF0",
+      "#111111"
+    ]
+  },
+  {
+    "autoColors":[
+      "#000000",
+      "#00e4ff",
+      "#0080ff",
+      "#6a3efc"
+    ],
+    "defaultColor":"#ffffff"
+  },
+  {
+    "autoColors":[
+      "#A2DA22",
+      "#c17d11",
+      "#204a87",
+      "#ec1a1a"
+    ],
+    "defaultColor":"#f3f3f3"
+  },
+  {
+    "autoColors":[
+      "#FF0000",
+      "#00FF00",
+      "#0000FF",
+      "#fff800"
+    ],
+    "defaultColor":"#f3f3f3"
+  },
+  {
+    "autoColors":[
+
+    ],
+    "defaultColor":"#f3f3f3",
+    "fixedColors":{
+      "True":"#ef2929"
+    }
+  }
+])";
 
 const char* const PALETTE =
 R"({
-    "autoColors": [
+  "autoColors":[
     "#199311",
     "#FF7700",
     "#8126C0",
@@ -37,8 +91,8 @@ R"({
     "#9D2323",
     "#798FF0",
     "#111111"
-    ],
-    "defaultColor": "#DDDDDD"
+  ],
+  "defaultColor":"#DDDDDD"
 })";
 } // namespace Defaults
 
