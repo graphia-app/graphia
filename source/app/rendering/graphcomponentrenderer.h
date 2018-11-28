f6fd0d6374908d1ed2104c943ed0ecf327d967cf
Sebastian Horsewell <whiteskh@googlemail.com>
2018-11-28 11:20:37 +0000

    Remove graphComponentRenderer copy constructor

--- source/app/rendering/graphcomponentrenderer.h
+++ source/app/rendering/graphcomponentrenderer.h
@@ -30,46 +30,6 @@ public:
     static const float COMFORTABLE_ZOOM_RADIUS;
 
     GraphComponentRenderer() = default;
-    GraphComponentRenderer(const GraphComponentRenderer& r) : _initialised(r._initialised),
-        _visible(r._visible), _frozen(r._frozen), _cleanupWhenThawed(r._cleanupWhenThawed),
-        _synchroniseWhenThawed(r._synchroniseWhenThawed), _viewData(r._viewData),
-        _savedViewData(r._savedViewData), _viewportWidth(r._viewportWidth), _viewportHeight(r._viewportHeight),
-        _dimensions(r._dimensions), _alpha(r._alpha), _trackFocus(r._trackFocus), _targetZoomDistance(r._targetZoomDistance),
-        _moveFocusToCentreOfComponentLater(r._moveFocusToCentreOfComponentLater), _componentId(r._componentId),
-        _nodeIds(r._nodeIds), _edges(r._edges), _fovx(r._fovx), _fovy(r._fovy), _graphModel(r._graphModel),
-        _selectionManager(r._selectionManager), _entireComponentZoomDistance(r._entireComponentZoomDistance)
-    {}
-
-    GraphComponentRenderer& operator=(const GraphComponentRenderer& other)
-    {
-        if(&other == this)
-            return *this;
-
-        _initialised = other._initialised;
-        _visible = other._visible;
-        _frozen = other._frozen;
-        _cleanupWhenThawed = other._cleanupWhenThawed;
-        _synchroniseWhenThawed = other._synchroniseWhenThawed;
-        _viewData = other._viewData;
-        _savedViewData = other._savedViewData;
-        _viewportWidth = other._viewportWidth;
-        _viewportHeight = other._viewportHeight;
-        _dimensions = other._dimensions;
-        _alpha = other._alpha;
-        _trackFocus = other._trackFocus;
-        _targetZoomDistance = other._targetZoomDistance;
-        _moveFocusToCentreOfComponentLater = other._moveFocusToCentreOfComponentLater;
-        _componentId = other._componentId;
-        _nodeIds = other._nodeIds;
-        _edges = other._edges;
-        _fovx = other._fovx;
-        _fovy = other._fovy;
-        _graphModel = other._graphModel;
-        _selectionManager = other._selectionManager;
-        _entireComponentZoomDistance = other._entireComponentZoomDistance;
-
-        return *this;
-    }
 
     void initialise(GraphModel* graphModel, ComponentId componentId,
                     SelectionManager* selectionManager,
@@ -144,8 +104,6 @@ public:
     static float maxNodeDistanceFromPoint(const GraphModel& graphModel,
                                           const QVector3D& centre,
                                           const std::vector<NodeId>& nodeIds);
-
-    QMatrix4x4 projectionMatrixDebug() const;
     QRectF dimensions() const;
 
 private:
