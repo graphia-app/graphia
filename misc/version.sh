67e5ace93cdf91b2c982a7f3dd016512c8a36486
Tim Angus <tim@ngus.net>
2019-01-14 17:25:28 +0000

    Improve version.sh

--- misc/version.sh
+++ misc/version.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-TAG=$(git describe --always)
+DESCRIPTION=$(git describe --always)
+TAG=$(git describe --abbrev=0)
+COMMITS_SINCE_TAG=$(git rev-list ${TAG}..HEAD --count)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "${BRANCH}" == "master" ];
+if [ "${BRANCH}" == "master" ] || [ "${COMMITS_SINCE_TAG}" == "0" ];
 then
-    echo ${TAG}
+    echo ${DESCRIPTION}
 else
-    echo ${TAG}-${BRANCH}
+    echo ${DESCRIPTION}-${BRANCH}
 fi
