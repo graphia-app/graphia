2fa998786fb40ec2c66bfb3cbbc18a5a9d6887df
Tim Angus <tim@ngus.net>
2019-03-14 11:28:42 +0000

    Add range checks to Document::move[Transform|Visualisation]

--- source/app/ui/document.cpp
+++ source/app/ui/document.cpp
@@ -1816,6 +1816,12 @@ void Document::moveGraphTransform(int from, int to)
     if(_graphModel == nullptr)
         return;
 
+    if(from < 0 || from >= _graphTransforms.size())
+        return;
+
+    if(to < 0 || to >= _graphTransforms.size())
+        return;
+
     QStringList newGraphTransforms = _graphTransforms;
     newGraphTransforms.move(from, to);
 
@@ -1941,6 +1947,12 @@ static bool visualisationsDiffer(const QStringList& a, const QStringList& b)
 void Document::moveVisualisation(int from, int to)
 {
     if(_graphModel == nullptr)
+        return;
+
+    if(from < 0 || from >= _graphTransforms.size())
+        return;
+
+    if(to < 0 || to >= _graphTransforms.size())
         return;
 
     QStringList newVisualisations = _visualisations;
