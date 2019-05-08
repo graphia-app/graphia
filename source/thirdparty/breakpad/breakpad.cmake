67ec93c2585be2bbf26271c4ce5381540bb0c291
Tim Angus <tim@ngus.net>
2019-05-08 16:02:52 +0100

    Add some more of the Breakpad source, so we can decode dmp files

--- source/thirdparty/breakpad/breakpad.cmake
+++ source/thirdparty/breakpad/breakpad.cmake
@@ -59,10 +59,42 @@ if(MSVC)
     )
 endif()
 
+list(APPEND BREAKPAD_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/basic_code_modules.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/call_stack.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/cfi_frame_info.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/disassembler_x86.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/dump_context.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/dump_object.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/exploitability.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/exploitability_linux.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/exploitability_win.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/logging.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/minidump_processor.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/pathname_stripper.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/process_state.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/proc_maps_linux.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/source_line_resolver_base.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stack_frame_symbolizer.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalk_common.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_amd64.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_arm64.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_arm.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_mips.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_ppc64.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_ppc.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_sparc.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/stackwalker_x86.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/symbolic_constants_win.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/tokenize.cc
+)
+
 list(APPEND BREAKPAD_HEADERS
     ${CMAKE_CURRENT_LIST_DIR}/exceptionrecord.h
     ${CMAKE_CURRENT_LIST_DIR}/crashhandler.h
 )
+
 list(APPEND BREAKPAD_SOURCES ${CMAKE_CURRENT_LIST_DIR}/crashhandler.cpp)
 
 if(UNITY_BUILD)
@@ -70,4 +102,24 @@ if(UNITY_BUILD)
 endif()
 
 list(APPEND STATIC_THIRDPARTY_HEADERS ${BREAKPAD_HEADERS})
-list(APPEND STATIC_THIRDPARTY_SOURCES ${BREAKPAD_SOURCES})
+list(APPEND STATIC_THIRDPARTY_SOURCES
+    ${BREAKPAD_SOURCES}
+
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/basic_source_line_resolver.cc
+    ${CMAKE_CURRENT_LIST_DIR}/src/processor/minidump.cc
+
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_implicit.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_insn.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_invariant.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_modrm.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_opcode_tables.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_operand.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_reg.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/ia32_settings.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_disasm.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_format.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_imm.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_insn.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_misc.c
+    ${CMAKE_CURRENT_LIST_DIR}/src/third_party/libdisasm/x86_operand_list.c
+)
--- source/thirdparty/thirdparty_headers.cmake
+++ source/thirdparty/thirdparty_headers.cmake
@@ -7,6 +7,8 @@ include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/hdf5)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/matio)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/zlib)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/qcustomplot)
+include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/breakpad/src)
 
 add_definitions(-DQCUSTOMPLOT_USE_OPENGL -DQCUSTOMPLOT_USE_LIBRARY)
 add_definitions(-DCRYPTOPP_DISABLE_ASM)
+add_definitions(-DBPLOG_MINIMUM_SEVERITY=SEVERITY_CRITICAL)
