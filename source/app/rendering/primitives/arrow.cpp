c75849be9face824afb949d9f0765a5c85e95d64
Tim Angus <tim@ngus.net>
2019-08-05 15:23:42 +0100

    Fix warnings in arrow.cpp

--- source/app/rendering/primitives/arrow.cpp
+++ source/app/rendering/primitives/arrow.cpp
@@ -258,7 +258,7 @@ void Arrow::generateVertexData(std::vector<float>& vertices, std::vector<float>&
 
     index = 0;
 
-    for(size_t slice = 0U; slice < _slices; slice++)
+    for(unsigned int slice = 0U; slice < _slices; slice++)
     {
         auto baseIndex = slice * 4;
 
