3fa715b07c72f630591ad225611fa3f705bc446f
Tim Angus <tim@ngus.net>
2020-11-11 15:08:34 +0000

    Simplify (and fix) doAsync

--- source/shared/utils/doasyncthen.h
+++ source/shared/utils/doasyncthen.h
@@ -30,47 +30,34 @@ namespace u
     class _Then
     {
     private:
-        QFutureWatcher<AsyncFnResult>* _watcher = nullptr;
-        bool _deleteWatcherOnDestruction = true;
+        QFuture<AsyncFnResult> _future;
 
     public:
-        explicit _Then(QFutureWatcher<AsyncFnResult>* watcher) :
-            _watcher(watcher)
+        explicit _Then(QFuture<AsyncFnResult>&& future) :
+            _future(std::move(future))
         {}
-
-        ~_Then()
-        {
-            if(_deleteWatcherOnDestruction)
-            {
-                // .then was never called
-                delete _watcher;
-            }
-        }
 
         template<typename ThenFn>
         void then(ThenFn&& thenFn)
         {
-            _deleteWatcherOnDestruction = false;
-
-            QObject::connect(_watcher, &QFutureWatcher<AsyncFnResult>::finished,
-            [thenFn, watcher = _watcher]
+            auto* watcher = new QFutureWatcher<AsyncFnResult>;
+            QObject::connect(watcher, &QFutureWatcher<AsyncFnResult>::finished,
+            [thenFn, watcher]
             {
                 thenFn(watcher->result());
                 watcher->deleteLater();
             });
+
+            watcher->setFuture(_future);
         }
     };
 
     template<typename AsyncFn>
     auto doAsync(AsyncFn&& thisFn)
     {
-        using AsyncFnResult = std::invoke_result_t<AsyncFn>;
-
         auto future = QtConcurrent::run(thisFn);
-        auto* watcher = new QFutureWatcher<AsyncFnResult>;
-        watcher->setFuture(future);
 
-        return _Then<AsyncFnResult>(watcher);
+        return _Then<std::invoke_result_t<AsyncFn>>(std::move(future));
     }
 } // namespace u
 
