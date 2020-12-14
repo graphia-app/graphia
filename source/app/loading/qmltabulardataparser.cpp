eb4f9fc856bd7c65edbadfd76ae9d30517b73fad
Tim Angus <tim@ngus.net>
2020-12-14 16:06:31 +0000

    Add QmlTabularDataParser

---
+++ source/app/loading/qmltabulardataparser.cpp
@@ -0,0 +1,258 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "qmltabulardataparser.h"
+
+#include "shared/loading/xlsxtabulardataparser.h"
+#include "shared/utils/scope_exit.h"
+#include "shared/utils/container.h"
+
+#include <QFileInfo>
+#include <QFuture>
+#include <QtConcurrent/QtConcurrent>
+
+#include <map>
+
+QmlTabularDataHeaderModel::QmlTabularDataHeaderModel(const QmlTabularDataParser* parser,
+    ValueType valueTypes, const QStringList& skip) :
+    QAbstractListModel(),
+    _parser(parser)
+{
+    _columnIndices.reserve(_parser->_data.numColumns());
+
+    for(size_t column = 0; column < _parser->_data.numColumns(); column++)
+    {
+        auto type = _parser->_columnTypeIdentities.at(column).type();
+
+        if(type == TypeIdentity::Type::String && !(valueTypes & ValueType::String))
+            continue;
+
+        if(type == TypeIdentity::Type::Int && !(valueTypes & ValueType::Int))
+            continue;
+
+        if(type == TypeIdentity::Type::Float && !(valueTypes & ValueType::Float))
+            continue;
+
+        const auto& value = _parser->_data.valueAt(column, 0);
+
+        if(value.isEmpty())
+            continue;
+
+        if(!skip.isEmpty() && skip.contains(value))
+            continue;
+
+        _columnIndices.emplace_back(column);
+    }
+}
+
+int QmlTabularDataHeaderModel::rowCount(const QModelIndex&) const
+{
+    return static_cast<int>(_columnIndices.size());
+}
+
+QVariant QmlTabularDataHeaderModel::data(const QModelIndex& index, int role) const
+{
+    // Rows in the model correspond to columns in the data table
+    auto columnIndicesIndex = static_cast<size_t>(index.row());
+
+    if(columnIndicesIndex >= _columnIndices.size())
+        return {};
+
+    auto columnIndex = _columnIndices.at(columnIndicesIndex);
+
+    if(role == Qt::DisplayRole)
+        return _parser->_data.valueAt(columnIndex, 0);
+
+    if(role == Roles::ColumnIndex)
+        return QVariant(static_cast<int>(columnIndex));
+
+    return {};
+}
+
+QHash<int, QByteArray> QmlTabularDataHeaderModel::roleNames() const
+{
+    auto names = QAbstractItemModel::roleNames();
+
+    names[Roles::ColumnIndex] = "columnIndex";
+
+    return names;
+}
+
+int QmlTabularDataHeaderModel::columnIndexFor(const QModelIndex& index) const
+{
+    return data(index, Roles::ColumnIndex).toInt();
+}
+
+QModelIndex QmlTabularDataHeaderModel::indexOf(int columnIndex) const
+{
+    auto row = u::indexOf(_columnIndices, columnIndex);
+
+    if(row < 0)
+        return {};
+
+    return index(row);
+}
+
+QmlTabularDataParser::QmlTabularDataParser()
+{
+    connect(&_dataParserWatcher, &QFutureWatcher<void>::started, this, &QmlTabularDataParser::busyChanged);
+    connect(&_dataParserWatcher, &QFutureWatcher<void>::finished, this, &QmlTabularDataParser::busyChanged);
+    connect(&_dataParserWatcher, &QFutureWatcher<void>::finished, this, &QmlTabularDataParser::onDataLoaded);
+    connect(&_dataParserWatcher, &QFutureWatcher<void>::finished, this, &QmlTabularDataParser::dataLoaded);
+
+    setProgressFn([this](int progress)
+    {
+        if(progress != _progress)
+        {
+            _progress = progress;
+            emit progressChanged();
+        }
+    });
+}
+
+QmlTabularDataParser::~QmlTabularDataParser()
+{
+    _dataParserWatcher.waitForFinished();
+}
+
+void QmlTabularDataParser::updateColumnTypes()
+{
+    _columnTypeIdentities = _data.typeIdentities(this);
+}
+
+bool QmlTabularDataParser::parse(const QUrl& fileUrl)
+{
+    reset();
+
+    QFuture<void> future = QtConcurrent::run([this, fileUrl]
+    {
+        if(fileUrl.isEmpty())
+            return;
+
+        auto tryToParseUsing = [fileUrl, this](auto&& parser)
+        {
+            _data.reset();
+            _cancellableParser = &parser;
+            auto atExit = std::experimental::make_scope_exit([this] { _cancellableParser = nullptr; });
+
+            if(!parser.canLoad(fileUrl))
+                return false;
+
+            parser.setProgressFn([this](int progress) { setProgress(progress); });
+
+            if(!parser.parse(fileUrl))
+                return false;
+
+            _data = std::move(parser.tabularData());
+            updateColumnTypes();
+            emit dataChanged();
+
+            return true;
+        };
+
+        std::map<QString, std::function<bool()>> parsers =
+        {
+            {QStringLiteral("csv"),     [&]{ return tryToParseUsing(CsvFileParser()); }},
+            {QStringLiteral("tsv"),     [&]{ return tryToParseUsing(TsvFileParser()); }},
+            {QStringLiteral("ssv"),     [&]{ return tryToParseUsing(SsvFileParser()); }},
+            {QStringLiteral("xlsx"),    [&]{ return tryToParseUsing(XlsxTabularDataParser()); }}
+        };
+
+        auto extension = QFileInfo(fileUrl.toLocalFile()).suffix();
+        bool success = false;
+
+        if(u::contains(parsers, extension))
+        {
+            const auto& parser = parsers.at(extension);
+            success = parser();
+        }
+
+        if(!success)
+        {
+            // Try all the other parsers, in case the user used the wrong extension
+            for(const auto& [parserExtension, parser] : parsers)
+            {
+                if(parserExtension == extension)
+                    continue;
+
+                if(parser())
+                    break;
+            }
+        }
+
+        setProgress(-1);
+    });
+
+    _dataParserWatcher.setFuture(future);
+    return true;
+}
+
+void QmlTabularDataParser::cancelParse()
+{
+    if(_cancellableParser != nullptr)
+        _cancellableParser.load()->cancel();
+
+    cancel();
+}
+
+void QmlTabularDataParser::reset()
+{
+    if(!_data.empty())
+    {
+        _data.reset();
+        emit dataChanged();
+    }
+
+    if(_progress != -1)
+    {
+        _progress = -1;
+        emit progressChanged();
+    }
+
+    if(_complete)
+    {
+        _complete = false;
+        emit completeChanged();
+    }
+
+    if(_failed)
+    {
+        _failed = false;
+        emit failedChanged();
+    }
+}
+
+QmlTabularDataHeaderModel* QmlTabularDataParser::headers(int _valueTypes, const QStringList& skip) const
+{
+    auto valueTypes = static_cast<ValueType>(_valueTypes);
+
+    // Caller takes ownership (usually QML/JS)
+    return new QmlTabularDataHeaderModel(this, valueTypes, skip);
+}
+
+void QmlTabularDataParser::onDataLoaded()
+{
+    if(_data.empty())
+    {
+        _failed = true;
+        emit failedChanged();
+    }
+
+    _complete = true;
+    emit completeChanged();
+}
---
+++ source/app/loading/qmltabulardataparser.h
@@ -0,0 +1,147 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef QMLTABULARDATAPARSER_H
+#define QMLTABULARDATAPARSER_H
+
+#include "shared/utils/cancellable.h"
+#include "shared/utils/progressable.h"
+#include "shared/utils/typeidentity.h"
+#include "shared/loading/tabulardata.h"
+#include "shared/attributes/valuetype.h"
+
+#include <QObject>
+#include <QFutureWatcher>
+#include <QQmlEngine>
+#include <QCoreApplication>
+#include <QAbstractListModel>
+#include <QVariantMap>
+
+#include <memory>
+#include <vector>
+#include <atomic>
+
+class QmlTabularDataParser;
+
+class QmlTabularDataHeaderModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    explicit QmlTabularDataHeaderModel(const QmlTabularDataParser* parser,
+        ValueType valueTypes = ValueType::All, const QStringList& skip = {});
+
+    enum Roles
+    {
+        ColumnIndex = Qt::UserRole + 1,
+    };
+
+    int rowCount(const QModelIndex&) const override;
+    QVariant data(const QModelIndex& index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE int columnIndexFor(const QModelIndex& index) const;
+    Q_INVOKABLE QModelIndex indexOf(int columnIndex) const;
+
+private:
+    const QmlTabularDataParser* _parser = nullptr;
+    std::vector<size_t> _columnIndices;
+};
+
+class QmlTabularDataParser : public QObject, public Cancellable, public Progressable
+{
+    friend class QmlTabularDataHeaderModel;
+
+    Q_OBJECT
+
+    Q_PROPERTY(TabularData* data READ dataPtr NOTIFY dataChanged)
+
+    Q_PROPERTY(bool busy READ busy NOTIFY busyChanged)
+    Q_PROPERTY(int progress MEMBER _progress WRITE setProgress NOTIFY progressChanged)
+    Q_PROPERTY(bool complete MEMBER _complete NOTIFY completeChanged)
+    Q_PROPERTY(bool failed MEMBER _failed NOTIFY failedChanged)
+
+private:
+    QFutureWatcher<void> _dataParserWatcher;
+    TabularData _data;
+
+    std::vector<TypeIdentity> _columnTypeIdentities;
+
+    int _progress = -1;
+    std::atomic<Cancellable*> _cancellableParser = nullptr;
+    bool _complete = false;
+    bool _failed = false;
+
+    TabularData* dataPtr() { return &_data; }
+
+    void updateColumnTypes();
+
+public:
+    QmlTabularDataParser();
+    virtual ~QmlTabularDataParser();
+
+    Q_INVOKABLE bool parse(const QUrl& fileUrl);
+    Q_INVOKABLE void cancelParse();
+    Q_INVOKABLE void reset();
+
+    Q_INVOKABLE QmlTabularDataHeaderModel* headers(
+        int _valueTypes = static_cast<int>(ValueType::All),
+        const QStringList& skip = {}) const;
+
+    bool busy() const { return _dataParserWatcher.isRunning(); }
+
+    static void registerQmlType()
+    {
+        static bool initialised = false;
+        if(initialised)
+            return;
+        initialised = true;
+        qmlRegisterType<QmlTabularDataParser>(APP_URI, APP_MAJOR_VERSION,
+            APP_MINOR_VERSION, "TabularDataParser");
+
+        qmlRegisterInterface<QmlTabularDataHeaderModel>("TabularDataHeaderModel"
+#if QT_VERSION >= QT_VERSION_CHECK(5, 15, 0)
+            , APP_MAJOR_VERSION
+#endif
+            );
+
+        qRegisterMetaType<TabularData*>("TabularData*");
+    }
+
+signals:
+    void dataChanged();
+
+    void busyChanged();
+    void progressChanged();
+    void completeChanged();
+    void failedChanged();
+
+    void dataLoaded();
+
+private slots:
+    void onDataLoaded();
+};
+
+static void tabularDataParserInitialiser()
+{
+    QmlTabularDataParser::registerQmlType();
+}
+
+Q_COREAPP_STARTUP_FUNCTION(tabularDataParserInitialiser)
+
+#endif // QMLTABULARDATAPARSER_H
