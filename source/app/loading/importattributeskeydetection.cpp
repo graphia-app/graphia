79548f1fab400f4d38273c2106365289d70b789a
Tim Angus <tim@ngus.net>
2020-12-14 18:20:16 +0000

    Add ImportAttributesKeyDetection

---
+++ source/app/loading/importattributeskeydetection.cpp
@@ -0,0 +1,95 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "importattributeskeydetection.h"
+
+#include <QFuture>
+#include <QtConcurrent/QtConcurrent>
+
+#include "ui/document.h"
+
+ImportAttributesKeyDetection::ImportAttributesKeyDetection()
+{
+    connect(&_watcher, &QFutureWatcher<void>::started, this, &ImportAttributesKeyDetection::busyChanged);
+    connect(&_watcher, &QFutureWatcher<void>::finished, this, &ImportAttributesKeyDetection::busyChanged);
+}
+
+ImportAttributesKeyDetection::~ImportAttributesKeyDetection()
+{
+    _watcher.waitForFinished();
+}
+
+void ImportAttributesKeyDetection::start()
+{
+    QFuture<void> future = QtConcurrent::run([this]
+    {
+        QString bestAttributeName;
+        size_t bestColumnIndex = 0;
+        int bestPercent = 0;
+
+        auto attributeNames = _document->availableAttributeNames(
+            static_cast<int>(ElementType::All), static_cast<int>(ValueType::String));
+
+        for(size_t columnIndex = 0; columnIndex < _tabularData->numColumns(); columnIndex++)
+        {
+            for(const auto& attributeName: attributeNames)
+            {
+                auto values = _document->allAttributeValues(attributeName);
+                auto percent = _tabularData->columnMatchPercentage(columnIndex, values);
+
+                // If we already have an equivalent match, prefer the one with the shorter attribute name
+                if(percent == bestPercent && attributeName.size() > bestAttributeName.size())
+                    continue;
+
+                // If we already have an equivalent match, prefer the earlier column
+                if(percent == bestPercent && columnIndex > bestColumnIndex)
+                    continue;
+
+                if(percent >= bestPercent)
+                {
+                    bestAttributeName = attributeName;
+                    bestColumnIndex = columnIndex;
+                    bestPercent = percent;
+                }
+
+                // Can't improve on 100%!
+                if(bestPercent >= 100)
+                    break;
+            }
+
+            if(bestPercent >= 100)
+                break;
+        }
+
+        _result.clear();
+        _result.insert(QStringLiteral("attributeName"), bestAttributeName);
+        _result.insert(QStringLiteral("column"), static_cast<int>(bestColumnIndex));
+        _result.insert(QStringLiteral("percent"), bestPercent);
+
+        emit resultChanged();
+    });
+
+    _watcher.setFuture(future);
+}
+
+void ImportAttributesKeyDetection::reset()
+{
+    _attributeValues = {};
+    _result = {};
+    emit resultChanged();
+}
---
+++ source/app/loading/importattributeskeydetection.h
@@ -0,0 +1,82 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IMPORTATTRIBUTESKEYDETECTION_H
+#define IMPORTATTRIBUTESKEYDETECTION_H
+
+#include "shared/loading/tabulardata.h"
+
+#include <QObject>
+#include <QCoreApplication>
+#include <QQmlEngine>
+#include <QFutureWatcher>
+#include <QVariantMap>
+
+class Document;
+
+class ImportAttributesKeyDetection : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(Document* document MEMBER _document)
+    Q_PROPERTY(QVariantMap attributeValues MEMBER _attributeValues)
+    Q_PROPERTY(TabularData* tabularData MEMBER _tabularData)
+    Q_PROPERTY(bool busy READ busy NOTIFY busyChanged)
+    Q_PROPERTY(QVariantMap result MEMBER _result NOTIFY resultChanged)
+
+private:
+    Document* _document = nullptr;
+    QVariantMap _attributeValues;
+    TabularData* _tabularData = nullptr;
+    QFutureWatcher<void> _watcher;
+    QVariantMap _result;
+
+public:
+    ImportAttributesKeyDetection();
+    virtual ~ImportAttributesKeyDetection();
+
+    Q_INVOKABLE void start();
+    Q_INVOKABLE void reset();
+
+    bool busy() const { return _watcher.isRunning(); }
+
+    static void registerQmlType()
+    {
+        static bool initialised = false;
+        if(initialised)
+            return;
+        initialised = true;
+        qmlRegisterType<ImportAttributesKeyDetection>(APP_URI, APP_MAJOR_VERSION,
+            APP_MINOR_VERSION, "ImportAttributesKeyDetection");
+
+        qRegisterMetaType<TabularData*>("TabularData*");
+    }
+
+signals:
+    void busyChanged();
+    void resultChanged();
+};
+
+static void keyDetectionInitialiser()
+{
+    ImportAttributesKeyDetection::registerQmlType();
+}
+
+Q_COREAPP_STARTUP_FUNCTION(keyDetectionInitialiser)
+
+#endif // IMPORTATTRIBUTESKEYDETECTION_H
