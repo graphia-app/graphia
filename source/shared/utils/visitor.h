34c022f55c1af61fcadd68fd8e9361bba3d133f7
Tim Angus <tim@ngus.net>
2021-02-26 14:26:21 +0000

    Add overloaded lambda functor Visitor template

---
+++ source/shared/utils/visitor.h
@@ -0,0 +1,41 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VISITOR_H
+#define VISITOR_H
+
+// This is a template to facilitate construction of a functor which
+// has multiple overloaded operator()s each with a different parameter type,
+// thus the object as a whole being a type matching functor. e.g.:
+//
+// Visitor
+// {
+//     [](int) { ... },
+//     [](double) { ... },
+//     [](const Struct&) { ... }
+// }
+
+template<class... Ts> struct Visitor : Ts... { using Ts::operator()...; };
+
+#if(__cplusplus < 202002L)
+template<class... Ts> Visitor(Ts...) -> Visitor<Ts...>;
+#else
+#warning Deduction guide can be removed
+#endif
+
+#endif // VISITOR_H
