c75e8a6e616fcf6aea807fed78c31d9cfc25ce2b
Tim Angus <tim@ngus.net>
2021-03-10 15:46:46 +0000

    Call setSaveRequired when enrichments are changed

--- source/app/ui/document.cpp
+++ source/app/ui/document.cpp
@@ -968,6 +968,8 @@ void Document::onLoadComplete(const QUrl&, bool success)
         setSaveRequired();
     });
 
+    connect(this, &Document::enrichmentTableModelsChanged, this, &Document::setSaveRequired);
+
     _graphModel->enableVisualUpdates();
 
     setStatus(QString(tr("Loaded %1 (%2 nodes, %3 edges, %4 components)")).arg(
