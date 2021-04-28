5261c8f68a7c4bb80702a35d3f890e8f6c1eed5c
Tim Angus <tim@ngus.net>
2021-04-28 17:03:50 +0100

    Add RemoveAttributesCommand

---
+++ source/app/commands/removeattributescommand.cpp
@@ -0,0 +1,106 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "removeattributescommand.h"
+
+#include "graph/graphmodel.h"
+
+#include "shared/loading/userelementdata.h"
+
+RemoveAttributesCommand::RemoveAttributesCommand(GraphModel* graphModel, QStringList attributeNames) :
+    _graphModel(graphModel), _attributeNames(std::move(attributeNames))
+{}
+
+QString RemoveAttributesCommand::description() const
+{
+    return _attributeNames.size() > 1 ? QObject::tr("Remove Attributes") : QObject::tr("Remove Attribute");
+}
+
+QString RemoveAttributesCommand::verb() const
+{
+    return _attributeNames.size() > 1 ? QObject::tr("Removing Attributes") : QObject::tr("Removing Attribute");
+}
+
+QString RemoveAttributesCommand::pastParticiple() const
+{
+    return _attributeNames.size() > 1 ?
+        QObject::tr("%1 Attributes Removed").arg(_attributeNames.size()) :
+        QObject::tr("Attribute %1 Removed").arg(_attributeNames.front());
+}
+
+QString RemoveAttributesCommand::debugDescription() const
+{
+    QString text = description();
+
+    for(const auto& attributeName : _attributeNames)
+        text.append(QStringLiteral("\n  %1").arg(attributeName));
+
+    return text;
+}
+
+bool RemoveAttributesCommand::execute()
+{
+    auto tracker = _graphModel->attributeChangesTracker();
+
+    for(auto attributeName : std::as_const(_attributeNames))
+    {
+        const auto* attribute = _graphModel->attributeByName(attributeName);
+        attributeName = Attribute::parseAttributeName(attributeName)._name;
+
+        Q_ASSERT(attribute->userDefined());
+        if(!attribute->userDefined())
+            continue;
+
+        if(attribute->elementType() == ElementType::Node)
+        {
+            auto* v = _graphModel->userNodeData().vector(attributeName);
+            _removedUserNodeDataVectors[attributeName] = std::move(*v);
+            _graphModel->userNodeData().remove(attributeName);
+        }
+        else if(attribute->elementType() == ElementType::Edge)
+        {
+            auto* v = _graphModel->userEdgeData().vector(attributeName);
+            _removedUserEdgeDataVectors[attributeName] = std::move(*v);
+            _graphModel->userEdgeData().remove(attributeName);
+        }
+
+        _graphModel->removeAttribute(attributeName);
+    }
+
+    return true;
+}
+
+void RemoveAttributesCommand::undo()
+{
+    auto tracker = _graphModel->attributeChangesTracker();
+
+    for(auto&& [vectorName, vector] : _removedUserNodeDataVectors)
+    {
+        _graphModel->userNodeData().add(vectorName);
+        _graphModel->userNodeData().setVector(vectorName, std::move(vector));
+    }
+
+    for(auto&& [vectorName, vector] : _removedUserEdgeDataVectors)
+    {
+        _graphModel->userEdgeData().add(vectorName);
+        _graphModel->userEdgeData().setVector(vectorName, std::move(vector));
+    }
+
+    _graphModel->userNodeData().exposeAsAttributes(*_graphModel);
+    _graphModel->userEdgeData().exposeAsAttributes(*_graphModel);
+}
---
+++ source/app/commands/removeattributescommand.h
@@ -0,0 +1,55 @@
+/* Copyright © 2013-2020 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef REMOVEATTRIBUTESCOMMAND_H
+#define REMOVEATTRIBUTESCOMMAND_H
+
+#include "shared/commands/icommand.h"
+
+#include "shared/loading/userdatavector.h"
+
+#include <QString>
+#include <QStringList>
+
+#include <map>
+
+class GraphModel;
+
+class RemoveAttributesCommand : public ICommand
+{
+private:
+    GraphModel* _graphModel = nullptr;
+
+    QStringList _attributeNames;
+    std::map<QString, UserDataVector> _removedUserNodeDataVectors;
+    std::map<QString, UserDataVector> _removedUserEdgeDataVectors;
+
+public:
+    RemoveAttributesCommand(GraphModel* graphModel, QStringList attributeNames);
+
+    QString description() const override;
+    QString verb() const override;
+    QString pastParticiple() const override;
+
+    QString debugDescription() const override;
+
+    bool execute() override;
+    void undo() override;
+};
+
+#endif // REMOVEATTRIBUTESCOMMAND_H
