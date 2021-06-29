90da0c574765f1be563fc6970aed649cbda7702d
Tim Angus <tim@ngus.net>
2021-06-29 13:13:19 +0100

    Suppress some cppcoreguidelines-avoid-non-const-global-variables

--- source/app/application.h
+++ source/app/application.h
@@ -236,7 +236,7 @@ private:
     static const int _majorVersion = APP_MAJOR_VERSION;
     static const int _minorVersion = APP_MINOR_VERSION;
 
-    static QString _appDir;
+    static QString _appDir; // NOLINT cppcoreguidelines-avoid-non-const-global-variables
 
     Updater _updater;
 
