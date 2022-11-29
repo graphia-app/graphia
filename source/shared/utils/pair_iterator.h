6a29f24e322da454c1955c444df2b6ec050b2f42
Tim Angus <tim@ngus.net>
2022-11-29 16:54:10 +0000

    Fix misc-confusable-identifiers

--- source/shared/utils/pair_iterator.h
+++ source/shared/utils/pair_iterator.h
@@ -23,7 +23,7 @@
 #include <utility>
 #include <cstddef>
 
-template<typename It, typename T, T It::value_type::*member>
+template<typename Iterator, typename T, T Iterator::value_type::*member>
 class pair_iterator
 {
 public:
@@ -33,7 +33,7 @@ public:
     using pointer = T*;
     using reference = T&;
 
-    explicit pair_iterator(It it) : _it(std::move(it)) {}
+    explicit pair_iterator(Iterator it) : _it(std::move(it)) {}
     auto& operator++() { return _it.operator++(); }
     auto operator++(int i) { return operator++(i); }
     auto& operator--() { return _it.operator--(); }
@@ -44,23 +44,23 @@ public:
     auto operator->() { return &((*_it).*member); }
 
 private:
-    It _it;
+    Iterator _it;
 };
 
-template<typename It> using pair_first_iterator =
-    pair_iterator<It, decltype(It::value_type::first), &It::value_type::first>;
-template<typename It> using pair_second_iterator =
-    pair_iterator<It, decltype(It::value_type::second), &It::value_type::second>;
+template<typename Iterator> using pair_first_iterator =
+    pair_iterator<Iterator, decltype(Iterator::value_type::first), &Iterator::value_type::first>;
+template<typename Iterator> using pair_second_iterator =
+    pair_iterator<Iterator, decltype(Iterator::value_type::second), &Iterator::value_type::second>;
 
-template<typename It>
-auto make_pair_first_iterator(const It& it) { return pair_first_iterator<It>(it); }
-template<typename It>
-auto make_pair_second_iterator(const It& it) { return pair_second_iterator<It>(it); }
+template<typename Iterator>
+auto make_pair_first_iterator(const Iterator& it) { return pair_first_iterator<Iterator>(it); }
+template<typename Iterator>
+auto make_pair_second_iterator(const Iterator& it) { return pair_second_iterator<Iterator>(it); }
 
-template<typename It>
-auto make_map_key_iterator(const It& it) { return make_pair_first_iterator(it); }
-template<typename It>
-auto make_map_value_iterator(const It& it) { return make_pair_second_iterator(it); }
+template<typename Iterator>
+auto make_map_key_iterator(const Iterator& it) { return make_pair_first_iterator(it); }
+template<typename Iterator>
+auto make_map_value_iterator(const Iterator& it) { return make_pair_second_iterator(it); }
 
 template<typename C>
 class key_wrapper
