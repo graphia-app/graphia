08c21b782f55b5fb73833400a21c3f4b1ff23558
Tim Angus <tim@ngus.net>
2023-11-02 14:42:15 +0000

    Include What You Use

---
+++ scripts/iwyu.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#
+# Copyright © 2013-2023 Graphia Technologies Ltd.
+#
+# This file is part of Graphia.
+#
+# Graphia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Graphia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+if [ -z ${BUILD_DIR} ]
+then
+    echo "BUILD_DIR not set"
+    exit 1
+fi
+
+. ${BUILD_DIR}/variables.sh
+
+include-what-you-use --version
+iwyu_tool.py -p ${BUILD_DIR} -j$(nproc --all) -- \
+    -Xiwyu --mapping_file=${IWYU_SOURCE}/qt5_11.imp 2>&1 | tee iwyu-${VERSION}.log
