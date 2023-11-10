50c68f62a98833da53bd71ceb48c253318283840
Tim Angus <tim@ngus.net>
2023-11-10 18:24:26 +0000

    Add SharedTextVisualisationChannel

---
+++ source/app/ui/visualisations/sharedtextvisualisationchannel.cpp
@@ -0,0 +1,104 @@
+/* Copyright © 2013-2023 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "sharedtextvisualisationchannel.h"
+
+#include "shared/graph/elementid.h"
+#include "shared/graph/elementid_containers.h"
+
+#include "graph/graphmodel.h"
+#include "graph/mutablegraph.h"
+
+#include "transform/transformedgraph.h"
+
+#include "transform/graphtransformconfig.h"
+#include "attributes/conditionfncreator.h"
+
+#include <QObject>
+
+TextVisuals SharedTextVisualisationChannel::textVisuals(const QString& attributeName,
+    const GraphModel& graphModel, const TransformedGraph& graph)
+{
+    Q_ASSERT(graphModel.attributeExists(attributeName));
+    if(!graphModel.attributeExists(attributeName))
+        return {};
+
+    TextVisuals textVisuals;
+
+    const auto* attribute = graphModel.attributeByName(attributeName);
+    auto contractedGraph = graph.mutableGraph();
+
+    GraphTransformConfig::TerminalCondition condition
+    {
+        u"$source.%1"_s.arg(attributeName), //FIXME needs parsed name?
+        ConditionFnOp::Equality::Equal,
+        u"$target.%1"_s.arg(attributeName),
+    };
+
+    auto conditionFn = CreateConditionFnFor::edge(graphModel, condition);
+    Q_ASSERT(conditionFn != nullptr);
+    if(conditionFn == nullptr)
+        return {};
+
+    EdgeIdSet edgeIdsToContract;
+
+    for(const EdgeId edgeId : contractedGraph.edgeIds())
+    {
+        if(conditionFn(edgeId))
+            edgeIdsToContract.insert(edgeId);
+    }
+
+    contractedGraph.contractEdges(edgeIdsToContract);
+
+    for(const NodeId nodeId : contractedGraph.nodeIds())
+    {
+        TextVisual textVisual;
+        textVisual._text = attribute->stringValueOf(nodeId);
+
+        switch(contractedGraph.typeOf(nodeId))
+        {
+        case MultiElementType::Not:
+            textVisual._nodeIds = {nodeId};
+            break;
+
+        case MultiElementType::Head:
+        {
+            auto mergedNodeIds = contractedGraph.mergedNodeIdsForNodeId(nodeId);
+
+            textVisual._nodeIds.reserve(static_cast<size_t>(mergedNodeIds.size()));
+            for(const NodeId mergedNodeId : mergedNodeIds)
+                textVisual._nodeIds.push_back(mergedNodeId);
+
+            break;
+        }
+
+        case MultiElementType::Tail:
+            continue;
+        }
+
+        const ComponentId componentId = graphModel.graph().componentIdOfNode(nodeId);
+        textVisuals[componentId].push_back(textVisual);
+    }
+
+    return textVisuals;
+}
+
+QString SharedTextVisualisationChannel::description(ElementType, ValueType) const
+{
+    return QObject::tr("Groups of nodes that share an attribute value will be labelled as such.");
+}
---
+++ source/app/ui/visualisations/sharedtextvisualisationchannel.h
@@ -0,0 +1,44 @@
+/* Copyright © 2013-2023 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SHAREDTEXTVISUALISATIONCHANNEL_H
+#define SHAREDTEXTVISUALISATIONCHANNEL_H
+
+#include "visualisationchannel.h"
+
+class SharedTextVisualisationChannel : public VisualisationChannel
+{
+public:
+    using VisualisationChannel::VisualisationChannel;
+
+    // This visualisation is not applied to individual elements
+    void apply(double, ElementVisual&) const override {}
+    void apply(const QString&, ElementVisual&) const override {}
+
+    TextVisuals textVisuals(const QString& attributeName,
+        const GraphModel& graphModel, const TransformedGraph&) override;
+
+    bool appliesTo(ElementType elementType) const override { return elementType & ElementType::Node; }
+    bool supports(ValueType valueType) const override { return valueType == ValueType::String; }
+    bool requiresRange() const override { return false; }
+    bool allowsMapping() const override { return false; }
+
+    QString description(ElementType, ValueType) const override;
+};
+
+#endif // SHAREDTEXTVISUALISATIONCHANNEL_H
