3fe660ead7254294f52a32b41d09e664582c151b
Tim Angus <tim@ngus.net>
2023-11-10 18:06:58 +0000

    Add TextVisual

---
+++ source/app/ui/visualisations/textvisual.cpp
@@ -0,0 +1,39 @@
+/* Copyright © 2013-2023 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "textvisual.h"
+
+#include "maths/boundingsphere.h"
+#include "layout/nodepositions.h"
+
+void TextVisual::updatePositions(const NodePositions& nodePositions)
+{
+    // This must be called when the layout is paused or otherwise locked
+    std::vector<QVector3D> points;
+    points.reserve(_nodeIds.size());
+
+    for(const NodeId nodeId : _nodeIds)
+    {
+        auto nodePosition = nodePositions.get(nodeId);
+        points.push_back(nodePosition);
+    }
+
+    const BoundingSphere boundingSphere(points);
+    _centre = boundingSphere.centre();
+    _radius = boundingSphere.radius();
+}
---
+++ source/app/ui/visualisations/textvisual.h
@@ -0,0 +1,49 @@
+/* Copyright © 2013-2023 Graphia Technologies Ltd.
+ *
+ * This file is part of Graphia.
+ *
+ * Graphia is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graphia is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TEXTVISUAL_H
+#define TEXTVISUAL_H
+
+#include "shared/graph/elementid.h"
+
+#include <QString>
+#include <QColor>
+#include <QVector3D>
+
+#include <vector>
+#include <map>
+
+class NodePositions;
+
+struct TextVisual
+{
+    QString _text;
+    float _size = -1.0f;
+    QColor _color;
+
+    std::vector<NodeId> _nodeIds;
+
+    QVector3D _centre;
+    float _radius = 0.0f;
+
+    void updatePositions(const NodePositions& nodePositions);
+};
+
+using TextVisuals = std::map<ComponentId, std::vector<TextVisual>>;
+
+#endif // TEXTVISUAL_H
