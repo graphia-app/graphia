b8e70db04a96c2ef0d63a3350d9a62c6d3995cc0
Tim Angus <tim@ngus.net>
2025-04-22 14:13:48 +0100

    Convert Windows build and installer batch scripts to Powershell
    (cherry picked from commit f816f1cdc13896b65ac9f3098980193433a7ef06)

---
+++ scripts/windows-build.ps1
@@ -0,0 +1,58 @@
+# Copyright © 2013-2025 Tim Angus
+# Copyright © 2013-2025 Tom Freeman
+#
+# This file is part of Graphia.
+#
+# Graphia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Graphia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+
+$ErrorActionPreference = 'Stop'
+
+. .\scripts\defaults.ps1
+
+$BUILD_DIR = "build"
+
+if(Test-Path $BUILD_DIR)
+{
+    Remove-Item -Recurse -Force $BUILD_DIR
+}
+New-Item -ItemType Directory -Path $BUILD_DIR | Out-Null
+
+Push-Location $BUILD_DIR
+
+cmake --version
+
+cmake -DCMAKE_UNITY_BUILD="$Env:UNITY_BUILD" `
+    -DCMAKE_BUILD_TYPE="$Env:BUILD_TYPE" `
+    -GNinja `
+    -DCMAKE_C_COMPILER="$Env:CC" `
+    -DCMAKE_CXX_COMPILER="$Env:CXX" `
+    ..
+
+Get-Content .\variables.ps1
+. .\variables.ps1
+
+cmake --build . --target all 2>&1 | Tee-Object "compiler-${VERSION}.log"
+
+Pop-Location
+
+$dumpSyms = "source\thirdparty\breakpad\src\tools\windows\binaries\dump_syms.exe"
+
+& $dumpSyms "$BUILD_DIR\${PRODUCT_NAME}.exe" > "$BUILD_DIR\${PRODUCT_NAME}.sym"
+& $dumpSyms "$BUILD_DIR\thirdparty.dll" > "$BUILD_DIR\thirdparty.dll.sym"
+
+Get-ChildItem "$BUILD_DIR\plugins\*.dll" | ForEach-Object {
+    $dllPath = $_.FullName
+    $symPath = "$($_.DirectoryName)\$($_.BaseName).sym"
+    & $dumpSyms $dllPath > $symPath
+}
