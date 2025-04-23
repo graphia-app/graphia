5a7a4b6542fb3e9e89dbd12d0539f3f3f48ee7a8
Tim Angus <tim@ngus.net>
2025-04-23 18:14:57 +0100

    Replace code signing on Windows with SignPath based solution

---
+++ installers/windows/make-installer.ps1
@@ -0,0 +1,31 @@
+# Copyright © 2013-2025 Tim Angus
+# Copyright © 2013-2025 Tom Freeman
+#
+# This file is part of Graphia.
+#
+# Graphia is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Graphia is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
+
+$ErrorActionPreference = 'Stop'
+
+$BUILD_DIR = "build"
+$INSTALLER_DIR = "installer"
+
+Get-Content ".\$BUILD_DIR\variables.ps1"
+. ".\$BUILD_DIR\variables.ps1"
+
+$MAKENSIS_ARGS = Get-Content "$BUILD_DIR\makensis_args.txt"
+
+Write-Host "------ making installer"
+$INSTALLER_EXE = "$INSTALLER_DIR\${PRODUCT_NAME}-${VERSION}-installer.exe"
+& makensis @MAKENSIS_ARGS "/XOutFile $INSTALLER_EXE" "installers\windows\installer.nsi"
