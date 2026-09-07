graphia-app/graphia

commit c4d49de51a04b661b4425bcc1bb473f75a0baba7

Make all the js libraries .pragma library

source/app/ui/qml/Graphia/TransformConfig.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman

source/app/ui/qml/Graphia/Utils/AttributeUtils.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman

source/app/ui/qml/Graphia/Utils/Constants.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman
@@ -17,8 +19,6 @@
  * You should have received a copy of the GNU General Public License
  * along with Graphia.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-.pragma library
 
 let margin = 8;
 let spacing = 8;

source/app/ui/qml/Graphia/Utils/MenuUtils.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman

source/app/ui/qml/Graphia/Utils/Utils.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman

source/app/ui/qml/Graphia/VisualisationUtils.js
@@ -1,3 +1,5 @@
+.pragma library
+
 /*
  * Copyright © 2013-2025 Tim Angus
  * Copyright © 2013-2025 Tom Freeman

commit 4e7fd461f927cefe23c1a00f1f42a26ff2e3e3af

Fix reference errors caused by c4d49de51

source/app/ui/qml/Graphia/Transform.qml
@@ -298,11 +298,12 @@ Item
                     setAlertIcon(transformInfo);
                 }
 
-                let transformConfig = new TransformConfig.Create(index, document.parseGraphTransform(value));
+                let transformConfig = new TransformConfig.Create(index, document, value);
                 flags = transformConfig.flags;
                 template = transformConfig.template;
 
-                transformConfig.toComponents(document, expression, isFlagSet("locked"), updateExpression);
+                transformConfig.toComponents(document, expression, root,
+                    isFlagSet("locked"), updateExpression);
                 _parameterComponents = transformConfig.parameters;
             }
             else
@@ -324,7 +325,7 @@ Item
 
                 TransformConfig.addLabelTo(
                     (detail.length > 0 ? detail + ": " : "Invalid Expression: ") +
-                    Utils.addSlashes(value), expression);
+                    Utils.addSlashes(value), expression, root);
             }
 
             enabledMenuItem.checked = !isFlagSet("disabled");

source/app/ui/qml/Graphia/TransformConfig.js
@@ -47,14 +47,17 @@ function createTransformParameter(document, parent, parameterData, onParameterCh
     return object;
 }
 
-function addLabelTo(text, parent)
+function addLabelTo(text, parent, transformItem)
 {
     text = text.trim();
-    Qt.createQmlObject("import QtQuick\n" +
+
+    let label = Qt.createQmlObject("import QtQuick\n" +
         "import QtQuick.Controls\n" +
         "Label { text: \"" +
         GraphiaUtils.Utils.normaliseWhitespace(text) +
-        "\"; color: root.textColor }", parent);
+        "\" }", parent);
+
+    label.color = Qt.binding(function() { return transformItem.textColor; });
 }
 
 function sanitiseOp(text)
@@ -85,9 +88,10 @@ function sanitiseOp(text)
     return text;
 }
 
-function Create(transformIndex, transform)
+function Create(transformIndex, document, transformString)
 {
     let parameterIndex = 1;
+    let transform = document.parseGraphTransform(transformString);
 
     this.action = transform.action;
     this.flags = transform.flags;
@@ -216,13 +220,13 @@ function Create(transformIndex, transform)
     }
 
     this.toComponents =
-    function(document, parent, locked, onParameterChanged)
+    function(document, parent, transformItem, locked, onParameterChanged)
     {
         let labelText = "";
 
         function addLabel()
         {
-            addLabelTo(labelText, parent);
+            addLabelTo(labelText, parent, transformItem);
             labelText = "";
         }
 