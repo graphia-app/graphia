92f22d31851d81a82a52a2c89309fca2a9c7d296
Tim Angus <tim@ngus.net>
2026-09-22 23:07:18 +0100

    Add progress reporting to clazy.sh

--- scripts/clazy.sh
+++ scripts/clazy.sh
@@ -96,7 +96,6 @@ CHECKS="-checks=\"${LEVEL0},${LEVEL1},${LEVEL2},${MANUAL}\""
 ${CLAZY} --version
 
 parallel -k -n1 -P$(nproc --all) \
-  ${CLAZY} --standalone -p compile_commands.json \
-  --ignore-dirs="\"(\/usr|thirdparty|\/Qt\/[0-9])\"" \
-  ${CHECKS} {} \
+  "echo [{#}/{= \$_=total_jobs() =}] {} && ${CLAZY} --standalone -p compile_commands.json \
+  --ignore-dirs='(\/usr|thirdparty|\/Qt\/[0-9])' ${CHECKS} {}" \
   ::: ${CPP_FILES} 2>&1 | tee clazy-${VERSION}.log
